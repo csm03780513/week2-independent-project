4c722c7508a4411f5a120953853cfc79fc578504
rmurray <rmurray@safaricom.co.ke>
2024-07-12 20:55:53 +0300

    add get element id by akan-form

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("akan-form")
+    .addEventListener("submit", function(event) {
+        event.preventDefault();
+        const birthday = document.getElementById("birthday").value;
+        const gender = document.getElementById("gender").value;
+    }
+);
